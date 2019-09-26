9d6f1821aa2b343de58295fa5d95e1e3f7953587
Jody Gustafson <jgustafson@hagerty.com>
2019-09-26 12:47:39 -0400

    Added validation

--- src/handlers/newMatchHandler.ts
+++ src/handlers/newMatchHandler.ts
@@ -8,6 +8,9 @@ export default async function newMatchHandler(event: APIGatewayEvent, context: C
     const count = event.pathParameters["count"];
     const seedCount = count ? parseInt(count) : 3;
     console.log("Seed count: " + seedCount);
+    if (!Number.isFinite(seedCount) || seedCount < 3 || seedCount > 6) {
+        return failure("Count must be a number from 3 to 6");
+    }
 
     const repo = new MatchRepository(new DynamoDbDataStore());
     let matchInfo = await repo.createMatch(seedCount);
